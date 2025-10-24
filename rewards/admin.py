3a231df82a658aa63fb2b77007c54029965a9aa1
Fatema4567pranti <fatemarahmanpranti@gmail.com>
2025-10-24 20:29:32 +0600

    badges

--- rewards/admin.py
+++ rewards/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Badge, UserProgress, UserBadge
 
-# Register your models here.
+admin.site.register(Badge)
+admin.site.register(UserProgress)
+admin.site.register(UserBadge)
--- rewards/signals.py
+++ rewards/signals.py
@@ -1,21 +1,45 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import UserProgress, UserBadge
+from .models import UserProgress, UserBadge, Badge
 from community.models import Post
 from wellness.models import MoodEntry
 
+def check_and_award_badges(user_progress):
+    current_xp = user_progress.total_xp
 
-@receiver(post_save, sender = Post)
+    eligible_badges = Badge.objects.filter(xp_reward__lte=current_xp).all()
+
+    for badge in eligible_badges:
+
+        has_badge = UserBadge.objects.filter(
+            user=user_progress.user,
+            badge=badge
+        ).exists()
+
+        if not has_badge:
+
+            UserBadge.objects.create(
+                user=user_progress.user,
+                badge=badge
+            )
+            print(f"DEBUG: Awarded badge: {badge.name} to {user_progress.user.username}")
+
+
+@receiver(post_save, sender=Post)
 def add_xp_on_community_post(sender, instance, created, **kwargs):
     if created:
         progress, _ = UserProgress.objects.get_or_create(user=instance.user)
         progress.total_xp += 15
         progress.save()
 
-@receiver(post_save, sender = MoodEntry)
-def add_xp_on_mood_log(sender, instance, created, **kwargs):
+        check_and_award_badges(progress)
 
+
+@receiver(post_save, sender=MoodEntry)
+def add_xp_on_mood_log(sender, instance, created, **kwargs):
     if created:
         progress, _ = UserProgress.objects.get_or_create(user=instance.user)
         progress.total_xp += 10
         progress.save()
+
+        check_and_award_badges(progress)
